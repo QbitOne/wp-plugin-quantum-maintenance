e3de601c95d2e24d3632b121a86ebb3ea9e0fc6e
Andreas Geyer <andreas@qbitone.de>
2021-11-21 11:43:13 +0100

    minor changes

--- public/public.class.php
+++ public/public.class.php
@@ -111,10 +111,13 @@ class Quaintenance_Public
 		$slug = $option['slug'] ?? '';
 		$mode = $option['mode'] ?? '';
 
+		global $post;
+		$current_slug = $post->post_name;
+
 		if (!($mode === 'enabled')) return;
 		if (is_admin() || current_user_can('manage_options')) return;
 
-		if (empty($slug) || is_singular()) :
+		if (empty($slug) || $slug === $current_slug) :
 			$this->deliver_maintenance_mode();
 			die();
 		endif;
