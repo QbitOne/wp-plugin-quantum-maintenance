654f08e9e2b9645888bb2b3a48c5ca986dfca9f7
Andreas Geyer <andreas@qbitone.de>
2021-11-21 12:08:58 +0100

    changes

---
+++ includes/update-checker.class.php
@@ -0,0 +1,80 @@
+<?php
+
+/**
+ * Update Checker Class
+ *
+ * @since 2.7.0
+ */
+
+if (!defined('ABSPATH')) exit; // Exit if accessed directly
+
+if (!class_exists('Quaintenance_Update_Checker')) :
+    /**
+     * Update Checker Class
+     */
+    class Quaintenance_Update_Checker
+    {
+        /**
+         * Vendor file for update checker
+         *
+         * @var string
+         */
+        private static $vendor_file = QUAINTENANCE_DIR . 'vendor/yahnis-elsts/plugin-update-checker/plugin-update-checker.php';
+
+        /**
+         * Remote path for the theme meta data
+         *
+         * @var string
+         */
+        private static $remote_path = 'https://qbitone.de/quaintenance.json';
+
+        /**
+         * Constructor
+         */
+        public function __construct()
+        {
+            if (file_exists(self::$vendor_file)) {
+
+                require self::$vendor_file;
+
+                $myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+                    self::$remote_path,
+                    QUAINTENANCE_DIR . 'quaintenance.php',
+                    'quaintenance'
+                );
+
+                if (defined('QT_ENV') && QT_ENV === 'dev') :
+                    add_action('admin_notices', [$this, 'action_admin_notices_activ_plugin_checker']);
+                endif;
+            } else {
+                add_action('admin_notices', [$this, 'action_admin_notices_no_composer']);
+            }
+        }
+
+        /**
+         * Prints admin screen notices.
+         * 
+         * @param none
+         * @return void
+         */
+        function action_admin_notices_no_composer(): void
+        {
+            echo '<div class="notice notice-info is-dismissible">';
+            echo '<p>Composer ist nicht installiert!</p>';
+            echo '</div>';
+        }
+
+        /**
+         * Prints admin screen notices.
+         * 
+         * @param none
+         * @return void
+         */
+        function action_admin_notices_activ_plugin_checker(): void
+        {
+            echo '<div class="notice notice-info is-dismissible">';
+            echo '<p>Update Checker ist aktiv</p>';
+            echo '</div>';
+        }
+    }
+endif;
